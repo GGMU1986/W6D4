b0f48f247dc7b0fa142cdf10a2a69f2fef43306e
gtsimis <gt2186@gmail.com>
2021-09-16 11:32:06 -0400

    Added uniqueness validation in model

--- ArtShare/app/models/artwork.rb
+++ ArtShare/app/models/artwork.rb
@@ -1,7 +1,7 @@
 class Artwork < ApplicationRecord
     validates :title, :image_url, :artist_id, presence: true
-    validates :title, uniqueness: { scope: :title_id, message: 'Artwork titles for a given artist should be unique!' }
-    
+    validates :title, uniqueness: { scope: :artist_id, message: 'Artwork titles for a given artist should be unique!' }
+
     belongs_to :artist,
         primary_key: :id,
         foreign_key: :artist_id,
