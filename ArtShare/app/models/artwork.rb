b394997ba8344e1eecb61533418cd32248cc9b26
gtsimis <gt2186@gmail.com>
2021-09-16 11:09:39 -0400

    Added validates

--- ArtShare/app/models/artwork.rb
+++ ArtShare/app/models/artwork.rb
@@ -1,6 +1,7 @@
 class Artwork < ApplicationRecord
     validates :title, :image_url, :artist_id, presence: true
-    #validates :country, uniqueness: { scope: [:medium, :another_medium] }
+    validates :title, uniqueness: { scope: :title_id, message: 'Artwork titles for a given artist should be unique!' }
+    
     belongs_to :artist,
         primary_key: :id,
         foreign_key: :artist_id,
